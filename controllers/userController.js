032e9447bab9bcabf308e5fcc91ff6590412292f
AMess33 <amess3309@gmail.com>
2023-04-16 22:18:28 -0500

    build user controller route functions

---
+++ controllers/userController.js
@@ -0,0 +1,35 @@
+const User = require("../models/User");
+
+module.exports = {
+  // get all users
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // get one user by id
+  async getOneUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId });
+      // if user not found throw error message
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  },
+  // create new user
+  async createUser(req, res) {
+    try {
+      const userData = await User.create(req.body);
+      res.json(userData);
+    } catch (err) {
+      res.status(500);
+    }
+  },
+};
